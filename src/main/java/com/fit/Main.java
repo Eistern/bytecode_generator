8ee5e790a56cb4ed7e617a0a18bff4b9778eadb2
d.zulin <d.zulin@g.nsu.ru>
2020-09-28 14:43:25 +0700

    Use more appropriate method

--- src/main/java/com/fit/Main.java
+++ src/main/java/com/fit/Main.java
@@ -30,26 +30,26 @@ public class Main {
 
     //var1 = new Random()
     methodVisitor.visitTypeInsn(Opcodes.NEW, "java/util/Random");
-    methodVisitor.visitIntInsn(Opcodes.ASTORE, 1);
-    methodVisitor.visitIntInsn(Opcodes.ALOAD, 1);
+    methodVisitor.visitVarInsn(Opcodes.ASTORE, 1);
+    methodVisitor.visitVarInsn(Opcodes.ALOAD, 1);
     methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/Random", "<init>", "()V", false);
 
     //var2 = var1.nextInt(100)
-    methodVisitor.visitIntInsn(Opcodes.ALOAD, 1);
+    methodVisitor.visitVarInsn(Opcodes.ALOAD, 1);
     methodVisitor.visitIntInsn(Opcodes.BIPUSH, 100);
     methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Random", "nextInt", "(I)I", false);
-    methodVisitor.visitIntInsn(Opcodes.ISTORE, 2);
+    methodVisitor.visitVarInsn(Opcodes.ISTORE, 2);
 
     //var3 = new Scanner(System.in)
     methodVisitor.visitTypeInsn(Opcodes.NEW, "java/util/Scanner");
-    methodVisitor.visitIntInsn(Opcodes.ASTORE, 3);
-    methodVisitor.visitIntInsn(Opcodes.ALOAD, 3);
+    methodVisitor.visitVarInsn(Opcodes.ASTORE, 3);
+    methodVisitor.visitVarInsn(Opcodes.ALOAD, 3);
     methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "in", "Ljava/io/InputStream;");
     methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/util/Scanner", "<init>", "(Ljava/io/InputStream;)V", false);
 
     //var4 = var2 + 1
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 2);
-    methodVisitor.visitIntInsn(Opcodes.ISTORE, 4);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
+    methodVisitor.visitVarInsn(Opcodes.ISTORE, 4);
     methodVisitor.visitIincInsn(4, 1);
 
     methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
@@ -61,20 +61,20 @@ public class Main {
 
     //while (var4 != var 2) { (while loop start)
     methodVisitor.visitLabel(whileStart);
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 4);
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 2);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 4);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
     methodVisitor.visitJumpInsn(Opcodes.IF_ICMPEQ, whileFinish);
 
     //var4 = var3.nextInt()
-    methodVisitor.visitIntInsn(Opcodes.ALOAD, 3);
+    methodVisitor.visitVarInsn(Opcodes.ALOAD, 3);
     methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Scanner", "nextInt", "()I", false);
-    methodVisitor.visitIntInsn(Opcodes.ISTORE, 4);
+    methodVisitor.visitVarInsn(Opcodes.ISTORE, 4);
 
     Label secondCheck = new Label();
 
     //if (var4 < var2) {sout("Lower");}
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 4);
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 2);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 4);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
     methodVisitor.visitJumpInsn(Opcodes.IF_ICMPGE, secondCheck);
     methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
     methodVisitor.visitLdcInsn("Greater");
@@ -82,8 +82,8 @@ public class Main {
 
     //if (var4 > var2) {sout("Greater");}
     methodVisitor.visitLabel(secondCheck);
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 4);
-    methodVisitor.visitIntInsn(Opcodes.ILOAD, 2);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 4);
+    methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
     methodVisitor.visitJumpInsn(Opcodes.IF_ICMPLE, whileStart);
     methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
     methodVisitor.visitLdcInsn("Lower");
